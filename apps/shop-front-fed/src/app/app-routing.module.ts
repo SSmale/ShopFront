487d609bda125fcc0bbe9cb662ec1005b8cbbb56
Simon Smale <simon@smalemail.net>
2020-04-07 10:40:15 +0100

    :sparkles: Feat: adds ng router config

---
+++ apps/shop-front-fed/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { OrderPageComponent } from './Pages/order-page/order-page.component';
+import { PageNoFoundComponent } from './Pages/page-no-found/page-no-found.component';
+import { OrderDetailComponent } from './Pages/order-detail/order-detail.component';
+
+const routes: Routes = [
+  { path: 'orders', component: OrderPageComponent },
+  { path: 'orders/:id', component: OrderDetailComponent },
+  { path: '**', component: PageNoFoundComponent }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
