9e7512d14fa50d18166682a0e5db433e0f7a5801
Simon Smale <simon@smalemail.net>
2020-04-07 11:57:17 +0100

    :sparkles: Fix: adds the missing spec file

--- apps/shop-front-fed/src/app/Pages/order-list/order-list.service.ts
+++ apps/shop-front-fed/src/app/Pages/order-list/order-list.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Observable } from 'rxjs';
+import { Product } from '../../../../../../libs/api-interfaces/src';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderListService {
 
-  constructor() { }
+  constructor(private readonly apiService: ApiService) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.apiService.get('/api/products')
+  }
 }
