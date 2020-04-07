6ef159309b0338df75b061e2f34137d279596f68
Simon Smale <simon@smalemail.net>
2020-04-07 10:39:02 +0100

    :sparkle: Feat: adds a page to list orders

---
+++ apps/shop-front-fed/src/app/Pages/order-page/order-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderPageComponent } from './order-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OrderPageComponent]
+})
+export class OrderPageModule { }
