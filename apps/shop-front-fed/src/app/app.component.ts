3dd1b509d786579dcf30829d3170c1cdc9359fdb
Simon Smale <simon@smalemail.net>
2020-04-07 15:15:29 +0100

    :fire: removes the starting code from the root app component

--- apps/shop-front-fed/src/app/app.component.ts
+++ apps/shop-front-fed/src/app/app.component.ts
@@ -1,25 +1,9 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from '@shop-front/api-interfaces';
 @Component({
   selector: 'shop-front-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  products$: Product[];
 
-  constructor(private http: HttpClient) {
-    this.products$ = [];
-
-    this.fetch();
-  }
-
-  fetch(): void {
-    this.http.get<Product[]>('api/products').subscribe(products => this.products$ = products);
-  }
-
-  addProduct(): void {
-    this.http.post('api/products', {}).subscribe(() => this.fetch());
-  }
 }
