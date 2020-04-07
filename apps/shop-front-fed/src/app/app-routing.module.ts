659c4e668a807612d5c1898481719fdbd29fa902
Simon Smale <simon@smalemail.net>
2020-04-07 11:54:37 +0100

    :sparkels: Feat: adds the new route for oder listing

--- apps/shop-front-fed/src/app/app-routing.module.ts
+++ apps/shop-front-fed/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { OrderPageComponent } from './Pages/order-page/order-page.component';
 import { PageNoFoundComponent } from './Pages/page-no-found/page-no-found.component';
 import { OrderDetailComponent } from './Pages/order-detail/order-detail.component';
+import { OrderListComponent } from './Pages/order-list/order-list.component';
 
 const routes: Routes = [
-  { path: 'orders', component: OrderPageComponent },
+  { path: 'order-form', component: OrderPageComponent },
+  { path: 'orders', component: OrderListComponent },
   { path: 'orders/:id', component: OrderDetailComponent },
   { path: '**', component: PageNoFoundComponent }
 ]
