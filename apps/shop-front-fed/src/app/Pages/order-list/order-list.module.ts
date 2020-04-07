53df29ad6ccb085c41e1dbdaf06603c0de758dc4
Simon Smale <simon@smalemail.net>
2020-04-07 17:12:38 +0100

    :sparkles: Feat: adds navigation from list to single

--- apps/shop-front-fed/src/app/Pages/order-list/order-list.module.ts
+++ apps/shop-front-fed/src/app/Pages/order-list/order-list.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderListService } from './order-list.service';
 import { OrderListComponent } from './order-list.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [OrderListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   providers: [OrderListService]
 })
