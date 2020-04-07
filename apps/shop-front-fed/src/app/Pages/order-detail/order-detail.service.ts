19cb6cac05444c9bb062bab6c5bc8b193ed43774
Simon Smale <simon@smalemail.net>
2020-04-07 16:25:54 +0100

    :sparkles: Feat: adds the order detail serivce

---
+++ apps/shop-front-fed/src/app/Pages/order-detail/order-detail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { Observable } from 'rxjs';
+import { Order } from '../../../../../../libs/api-interfaces/src';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailService {
+
+  constructor(private readonly apiService: ApiService) { }
+
+  getOrder(id: number): Observable<Order> {
+    return this.apiService.get(`/api/order/${id}`)
+  }
+
+}
