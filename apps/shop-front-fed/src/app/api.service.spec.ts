7af61c5606b556b8f855f47591e29042ace8be59
Simon Smale <simon@smalemail.net>
2020-04-10 09:15:05 +0100

    WIP: testing tests out

--- apps/shop-front-fed/src/app/api.service.spec.ts
+++ apps/shop-front-fed/src/app/api.service.spec.ts
@@ -1,16 +1,33 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
-
 describe('Service: App', () => {
+  let httpMock: HttpTestingController;
+  let service: ApiService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApiService]
+      imports: [HttpClientTestingModule],
+      providers: [ApiService],
     });
   });
 
-  it('should ...', inject([ApiService], (service: ApiService) => {
-    expect(service).toBeTruthy();
+  beforeEach(inject([ApiService, HttpTestingController], (_service, _httpMock) => {
+    service = _service;
+    httpMock = _httpMock;
   }));
+
+  it('should get the thing', () => {
+    service.get('/api/orders').subscribe(x => {
+      console.log(x);
+      expect(x).toBe(3);
+
+      const req = httpMock.expectOne('https://foo.bar.com/airports');
+
+      req.flush({});
+      httpMock.verify();
+    });
+  });
 });
--- apps/shop-front-fed/src/app/api.service.ts
+++ apps/shop-front-fed/src/app/api.service.ts
@@ -3,26 +3,24 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {}
 
   get(url: string): Observable<any> {
-    return this.http.get(url)
+    return this.http.get(url);
   }
 
   post(url: string, body?: any): Observable<any> {
-    return this.http.post(url, body)
+    return this.http.post(url, body);
   }
 
   delete(url: string): Observable<any> {
-    return this.http.delete(url)
+    return this.http.delete(url);
   }
 
   put(url: string, body?: any): Observable<any> {
-    return this.http.put(url, body)
+    return this.http.put(url, body);
   }
-
 }
