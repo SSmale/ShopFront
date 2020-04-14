02197cb1702b657d78dc63504218a2ad45e74772
Simon Smale <simon@smalemail.net>
2020-04-14 10:04:02 +0100

    :rotating_light: Chore(eslint): ran the file through the linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,122 +1,110 @@
 module.exports = {
-    "root": true,
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "project": "./tsconfig.json"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "@nrwl/nx"
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', '@nrwl/nx'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['**/*'],
+  rules: {
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'off',
+      {
+        accessibility: 'explicit',
+      },
     ],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
+    '@typescript-eslint/member-ordering': 'error',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/prefer-function-type': 'error',
+    '@typescript-eslint/unified-signatures': 'error',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@nrwl/nx/enforce-module-boundaries': [
+      'error',
+      {
+        allow: [],
+        depConstraints: [
+          {
+            sourceTag: '*',
+            onlyDependOnLibsWithTags: ['*'],
+          },
+        ],
+        enforceBuildableLibDependency: true,
+      },
     ],
-    "ignorePatterns": ["**/*"],
-    "rules": {
-        "prettier/prettier": [
-            "error"
+    'arrow-body-style': 'error',
+    camelcase: 'off',
+    'constructor-super': 'error',
+    'dot-notation': 'off',
+    eqeqeq: ['error', 'smart'],
+    'guard-for-in': 'error',
+    'id-blacklist': 'off',
+    'id-match': 'off',
+    'import/no-deprecated': 'warn',
+    'no-bitwise': 'error',
+    'no-caller': 'error',
+    'no-console': [
+      'error',
+      {
+        allow: [
+          'log',
+          'warn',
+          'dir',
+          'timeLog',
+          'assert',
+          'clear',
+          'count',
+          'countReset',
+          'group',
+          'groupEnd',
+          'table',
+          'dirxml',
+          'error',
+          'groupCollapsed',
+          'Console',
+          'profile',
+          'profileEnd',
+          'timeStamp',
+          'context',
         ],
-        "@typescript-eslint/class-name-casing": "error",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/consistent-type-definitions": "error",
-        "@typescript-eslint/explicit-member-accessibility": [
-            "off",
-            {
-                "accessibility": "explicit"
-            }
-        ],
-        "@typescript-eslint/member-ordering": "error",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-empty-interface": "error",
-        "@typescript-eslint/no-inferrable-types": "error",
-        "@typescript-eslint/no-misused-new": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        "@typescript-eslint/prefer-function-type": "error",
-        "@typescript-eslint/unified-signatures": "error",
-        "@typescript-eslint/no-parameter-properties": "off",
-        "@nrwl/nx/enforce-module-boundaries": [
-            "error",
-            {
-                "allow": [],
-                "depConstraints": [
-                    {
-                        "sourceTag": "*",
-                        "onlyDependOnLibsWithTags": [
-                            "*"
-                        ]
-                    }
-                ],
-                "enforceBuildableLibDependency": true
-            }
-        ],
-        "arrow-body-style": "error",
-        "camelcase": "off",
-        "constructor-super": "error",
-        "dot-notation": "off",
-        "eqeqeq": [
-            "error",
-            "smart"
-        ],
-        "guard-for-in": "error",
-        "id-blacklist": "off",
-        "id-match": "off",
-        "import/no-deprecated": "warn",
-        "no-bitwise": "error",
-        "no-caller": "error",
-        "no-console": [
-            "error",
-            {
-                "allow": [
-                    "log",
-                    "warn",
-                    "dir",
-                    "timeLog",
-                    "assert",
-                    "clear",
-                    "count",
-                    "countReset",
-                    "group",
-                    "groupEnd",
-                    "table",
-                    "dirxml",
-                    "error",
-                    "groupCollapsed",
-                    "Console",
-                    "profile",
-                    "profileEnd",
-                    "timeStamp",
-                    "context"
-                ]
-            }
-        ],
-        "no-debugger": "error",
-        "no-empty": "off",
-        "no-eval": "error",
-        "no-fallthrough": "error",
-        "no-new-wrappers": "error",
-        "no-restricted-imports": [
-            "error",
-            "rxjs/Rx"
-        ],
-        "no-shadow": [
-            "error",
-            {
-                "hoist": "all"
-            }
-        ],
-        "no-throw-literal": "error",
-        "no-undef-init": "error",
-        "no-underscore-dangle": "off",
-        "no-unused-expressions": "error",
-        "no-var": "error",
-        "prefer-const": "error",
-        "radix": "error",
-    }
-}
+      },
+    ],
+    'no-debugger': 'error',
+    'no-empty': 'off',
+    'no-eval': 'error',
+    'no-fallthrough': 'error',
+    'no-new-wrappers': 'error',
+    'no-restricted-imports': ['error', 'rxjs/Rx'],
+    'no-shadow': [
+      'error',
+      {
+        hoist: 'all',
+      },
+    ],
+    'no-throw-literal': 'error',
+    'no-undef-init': 'error',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    radix: 'error',
+  },
+};
