0bf4a8227c5a6498f564931117d4c7bce5c4f084
Simon Smale <simon@smalemail.net>
2020-04-14 15:47:32 +0100

    :sparkles: Feat: adds the api endpoints doe orderProducts entities

--- apps/api/src/app/orderProduct/orderProduct.controller.ts
+++ apps/api/src/app/orderProduct/orderProduct.controller.ts
@@ -1,15 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Put, Body, Post } from '@nestjs/common';
 import { OrderProduct } from './orderProduct.entity';
 import { OrderProductService } from './orderProduct.service';
+import { IOrderProduct } from '../../../../../libs/api-interfaces/src';
 
 @Controller('orderProduct')
 export class OrderProductController {
   constructor(private readonly orderProductService: OrderProductService) {}
 
-  @Get('orderProduct')
-  getorderProduct(): Promise<OrderProduct[]> {
-    console.log('orderProduct/orderProduct');
-
+  @Get('')
+  getorderProducts(): Promise<OrderProduct[]> {
     return this.orderProductService.findAll();
+  }
+
+  @Get('/:id')
+  getorderProduct(@Param('id') index: string): Promise<IOrderProduct> {
+    return this.orderProductService.findOne(index);
+  }
+
+  @Put('/:id')
+  updateOrderProduct(@Param() index: string, @Body() body: IOrderProduct): Promise<IOrderProduct> {
+    return this.updateOrderProduct(index, body);
+  }
+
+  @Post('')
+  addOrderProduct(@Body() body: IOrderProduct): Promise<IOrderProduct> {
+    return this.orderProductService.create(body);
   }
 }
